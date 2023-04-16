30a2a3942c123119f190598ba5028c50cb519220
Takashi Kitano <kitano@tr-imagination.com>
2023-04-16 16:54:55 +0900

    Update public.py
    get_shop_by_partner → get_shops_by_partner

--- pyshopee2/public.py
+++ pyshopee2/public.py
@@ -2,11 +2,11 @@ from .base import BaseModule
 
 
 class Public(BaseModule):
-    def get_shop_by_partner(self, **kwargs):
+    def get_shops_by_partner(self, **kwargs):
         """
 
         :param kwargs
         :return
 
         """
-        return self.client.execute("public/get_shop_by_partner", "GET", kwargs)
+        return self.client.execute("public/get_shops_by_partner", "GET", kwargs)
